e6e9515b1493abd75a4f9b9b52bee8cd14b044c1
Edilson Junior <edilson.junior@kiper.com.br>
2020-05-22 22:15:23 -0300

    feat: change post titles

--- pages/contact.js
+++ pages/contact.js
@@ -5,6 +5,7 @@ import Layout from '../components/layout';
 
 import Head from 'next/head';
 import { CMS_NAME } from '../lib/constants';
+import Header from '../components/header';
 
 export default function Index() {
   return (
@@ -14,7 +15,8 @@ export default function Index() {
           <title>{CMS_NAME}</title>
         </Head>
         <Container>
-          <Intro title={'Get in touch.'} />
+          <Header />
+          <Intro title={'Get in touch'} />
         </Container>
       </Layout>
     </>
